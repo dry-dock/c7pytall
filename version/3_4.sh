a7a07c20d596757fd387c4cba8288446ce30a6ab
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-25 16:23:25 +0530

    Create 3_4.sh

---
+++ version/3_4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+echo "================= Installing Python 3.4 ==================="
+
+ wget "https://www.python.org/ftp/python/3.4.8/Python-3.4.8.tgz"
+ tar -xf Python-3.4.8.tgz
+ cd Python-3.4.8
+ ./configure 
+ make
+ sudo make install
+
+echo "================= Successfully Installed Python 3.4 ==================="
