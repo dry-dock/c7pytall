5072aac3e141a42d7b8eb48b2cebdea602826764
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-25 17:41:46 +0530

    Update 2_4.sh

--- version/2_4.sh
+++ version/2_4.sh
@@ -1,12 +1,12 @@
  #!/bin/bash -e
  
- echo "================= Installing Python 2.4 ==================="
+echo "================= Installing Python 2.4 ==================="
  
- wget "https://www.python.org/ftp/python/2.4.6/Python-2.4.6.tgz"
- tar -xf Python-2.4.6.tgz
- cd Python-2.4.6
- ./configure
-	make install
+wget "https://www.python.org/ftp/python/2.4.6/Python-2.4.6.tgz"
+tar -xf Python-2.4.6.tgz
+cd Python-2.4.6
+./configure
+make install
 
 
 echo "================= Successfully Installed Python 2.4 ==================="
