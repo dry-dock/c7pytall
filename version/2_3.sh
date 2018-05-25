f186d340fb3a79e42233327011ddb688d9dff31a
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-25 19:49:30 +0530

    Update 2_3.sh

--- version/2_3.sh
+++ version/2_3.sh
@@ -6,7 +6,7 @@ wget "https://www.python.org/ftp/python/2.3.7/Python-2.3.7.tgz"
 tar -xf Python-2.3.7.tgz
 cd Python-2.3.7
 ./configure
-make install
+make altinstall
 
 
 echo "================= Successfully Installed Python 2.3 ==================="
