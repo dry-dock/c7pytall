5c1c6dc6e449579bbf27298af46ee234a6f373e6
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-25 17:13:11 +0530

    Update 2_5.sh

--- version/2_5.sh
+++ version/2_5.sh
@@ -2,6 +2,11 @@
 
 echo "================= Installing Python  ==================="
 
+wget "https://www.python.org/ftp/python/2.5.6/Python-2.5.6.tgz"
+tar -xf Python-2.5.6.tgz
+cd Python-2.5.6
+./configure
+make install
 
 
 echo "================= Successfully Installed Python  ==================="
