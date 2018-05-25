d7b5137c9059b696bd4644d77d7f96210c34bdbf
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-25 17:08:00 +0530

    Update 2_6.sh

--- version/2_6.sh
+++ version/2_6.sh
@@ -2,9 +2,9 @@
 
 echo "================= Installing Python 2.6 ==================="
 
- wget "https://www.python.org/ftp/python/2.6.6/Python-2.6.6.tgz"
- tar -xf Python-2.6.6.tgz
- cd Python-2.6.6
+ wget "https://www.python.org/ftp/python/2.6.9/Python-2.6.9.tgz"
+ tar -xf Python-2.6.9.tgz
+ cd Python-2.6.9
 ./configure
  make install
 
