417ce29d80ca73aabcb0db7d4d443315b40e57be
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-25 16:22:21 +0530

    Create 3_5.sh

---
+++ version/3_5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+echo "================= Installing Python 3.5 ==================="
+
+wget "https://www.python.org/ftp/python/3.5.5/Python-3.5.5.tgz"
+tar -xf Python-3.5.5.tgz
+cd  Python-3.5.5
+./configure
+make
+sudo make install
+
+
+echo "================= Successfully Installed Python 3.5 ==================="
