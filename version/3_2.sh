2df5319862f5bed9ccbb6eb264ecc16af29d8b5c
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-28 12:54:18 +0530

    Update 3_2.sh

--- version/3_2.sh
+++ version/3_2.sh
@@ -4,10 +4,15 @@ echo "================= Installing Python 3.2 ==================="
 
  wget "https://www.python.org/ftp/python/3.2.5/Python-3.2.5.tar.bz2"
  tar -xf Python-3.2.5.tar.bz2
- cd Python-3.2.5 
- ./configure 
- make 
- make altinstall
- ln -s /usr/local/bin/python3.2 /usr/bin/python3.2
+ rm Python-3.2.5.tar.bz2 
+ 
+ pushd Python-3.2.5 
+   ./configure 
+   make 
+   make altinstall
+   ln -s /usr/local/bin/python3.2 /usr/bin/python3.2
+popd 
+
+rm -rf Python-3.2.5 
 
 echo "================= Successfully Installed Python 3.2 ==================="
