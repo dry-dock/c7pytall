b78b86d366156f695d628f31eae6de0669542a6d
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-28 12:58:33 +0530

    Update 3_5.sh

--- version/3_5.sh
+++ version/3_5.sh
@@ -4,11 +4,15 @@ echo "================= Installing Python 3.5 ==================="
 
 wget "https://www.python.org/ftp/python/3.5.5/Python-3.5.5.tgz"
 tar -xf Python-3.5.5.tgz
-cd  Python-3.5.5
-./configure
-make
-make altinstall
-ln -s /usr/local/bin/python3.5 /usr/bin/python3.5 
+rm Python-3.5.5.tgz
 
+pushd Python-3.5.5
+  ./configure
+  make
+  make altinstall
+  ln -s /usr/local/bin/python3.5 /usr/bin/python3.5 
+popd
+
+rm -rf Python-3.5.5
 
 echo "================= Successfully Installed Python 3.5 ==================="
