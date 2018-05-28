5a2d37939854e084de504c9dac0dde159ce2396f
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-28 12:49:57 +0530

    Update 2_6.sh

--- version/2_6.sh
+++ version/2_6.sh
@@ -4,10 +4,14 @@ echo "================= Installing Python 2.6 ==================="
 
  wget "https://www.python.org/ftp/python/2.6.9/Python-2.6.9.tgz"
  tar -xf Python-2.6.9.tgz
- cd Python-2.6.9
-./configure
- make altinstall
- ln -s /usr/local/bin/python2.6 /usr/bin/python2.6
+ rm Python-2.6.9.tgz
+ 
+pushd Python-2.6.9
+  ./configure
+  make altinstall
+  ln -s /usr/local/bin/python2.6 /usr/bin/python2.6
+popd
 
+rm -rf Python-2.6.9
 
 echo "================= Successfully Installed Python 2.6 ==================="
