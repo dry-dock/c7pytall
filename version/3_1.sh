3b69726f2c4536b1fa617314045e23c1ed956c46
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-28 12:52:52 +0530

    Update 3_1.sh

--- version/3_1.sh
+++ version/3_1.sh
@@ -4,11 +4,15 @@ echo "================= Installing Python 3.1 ==================="
 
 wget "https://www.python.org/ftp/python/3.1.5/Python-3.1.5.tgz"
 tar -xf Python-3.1.5.tgz
-cd Python-3.1.5
-./configure
-make
-make altinstall
-ln -s /usr/local/bin/python3.1 /usr/bin/python3.1
+rm Python-3.1.5.tgz
 
+pushd Python-3.1.5
+  ./configure
+  make
+  make altinstall
+  ln -s /usr/local/bin/python3.1 /usr/bin/python3.1
+popd
+
+rm -rf Python-3.1.5
 
 echo "================= Successfully Installed Python 3.1 ==================="
