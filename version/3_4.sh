a841827d1ab617299726c17faad0c5e0f5382a6e
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-28 12:57:24 +0530

    Update 3_4.sh

--- version/3_4.sh
+++ version/3_4.sh
@@ -4,10 +4,15 @@ echo "================= Installing Python 3.4 ==================="
 
  wget "https://www.python.org/ftp/python/3.4.8/Python-3.4.8.tgz"
  tar -xf Python-3.4.8.tgz
- cd Python-3.4.8
- ./configure 
- make
- make altinstall
- ln -s /usr/local/bin/python3.4 /usr/bin/python3.4 
+ rm Python-3.4.8.tgz
+
+ pushd Python-3.4.8
+   ./configure 
+   make
+   make altinstall
+   ln -s /usr/local/bin/python3.4 /usr/bin/python3.4 
+popd
+
+rm -rf Python-3.4.8
 
 echo "================= Successfully Installed Python 3.4 ==================="
