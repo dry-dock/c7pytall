0dc74213cd0e63a9499483e3ac678be2c3f01822
vijayreddy1991 <vijayreddy.chenna@gmail.com>
2018-09-24 13:40:46 +0530

    update the latest versions

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-# Installing pip-18.0
+export PYTHON_PIP=18.0
 wget -nv https://bootstrap.pypa.io/3.3/get-pip.py
 python get-pip.py
-python -m pip install pip==18.0
+python -m pip install pip=="$PYTHON_PIP"
 
 pip install -U pip setuptools
 
