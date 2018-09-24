25181dc0f9359470e6bc9c7b482b217fe73327ea
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-09-24 13:29:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-# Installing pip-18.0
+# Installing pip-10.0.1
 wget -nv https://bootstrap.pypa.io/3.3/get-pip.py
 python get-pip.py
-python -m pip install pip==18.0
+python -m pip install pip==10.0.1
 
 pip install -U pip setuptools
 
