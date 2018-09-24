b324ee5b7da6209aff19d747099b4d6f8f890b60
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-09-24 13:53:07 +0530

    Update 2_7.sh

--- version/2_7.sh
+++ version/2_7.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
-export PYTHON_2_7_15_VERSION=2.7.15
-echo "================= Installing Python "$PYTHON_2_7_15_VERSION" ==================="
+export PYTHON_VERSION=2.7.15
+echo "================= Installing Python "$PYTHON_VERSION" ==================="
 yum install gcc openssl-devel bzip2-devel
 cd /usr/src
-wget https://www.python.org/ftp/python/"$PYTHON_2_7_15_VERSION"/Python-"$PYTHON_2_7_15_VERSION".tgz
-tar xzf Python-"$PYTHON_2_7_15_VERSION".tgz
-cd Python-"$PYTHON_2_7_15_VERSION"
+wget https://www.python.org/ftp/python/"$PYTHON_VERSION"/Python-"$PYTHON_VERSION".tgz
+tar xzf Python-"$PYTHON_VERSION".tgz
+cd Python-"$PYTHON_VERSION"
 ./configure --enable-optimizations
 make altinstall
-rm /usr/src/Python-"$PYTHON_2_7_15_VERSION".tgz
+rm /usr/src/Python-"$PYTHON_VERSION".tgz
 
 # Install virtualenv
 virtualenv -p python $HOME/venv/2.7
@@ -20,4 +20,4 @@ pip install pyopenssl ndg-httpsclient pyasn1
 pip install nose mock pytest coverage
 CFLAGS="-O0" pip install lxml
 deactivate
-echo "================= Successfully Installed Python "$PYTHON_2_7_15_VERSION" ==================="
+echo "================= Successfully Installed Python "$PYTHON_VERSION" ==================="
