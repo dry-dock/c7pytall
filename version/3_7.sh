5440fa0167e2ddf22f8a62fbc64eb27470fcaba3
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-09-24 13:55:07 +0530

    Update 3_7.sh

--- version/3_7.sh
+++ version/3_7.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -e
 
-export PYTHON_3_7_0_VERSION=3.7.0
-echo "================= Installing Python "$PYTHON_3_7_0_VERSION" =================="
+export PYTHON_VERSION=3.7.0
+echo "================= Installing Python "$PYTHON_VERSION" =================="
 
 yum install gcc openssl-devel bzip2-devel
 cd /usr/src
-wget https://www.python.org/ftp/python/"$PYTHON_3_7_0_VERSION"/Python-"$PYTHON_3_7_0_VERSION".tgz
-tar xzf Python-"$PYTHON_3_7_0_VERSION".tgz
-cd Python-"$PYTHON_3_7_0_VERSION"
+wget https://www.python.org/ftp/python/"$PYTHON_VERSION"/Python-"$PYTHON_VERSION".tgz
+tar xzf Python-"$PYTHON_VERSION".tgz
+cd Python-"$PYTHON_VERSION"
 ./configure --enable-optimizations
 make altinstall
-rm -f /usr/src/Python-"$PYTHON_3_7_0_VERSION".tgz
+rm -f /usr/src/Python-"$PYTHON_VERSION".tgz
 
 # Install virtualenv
-virtualenv -p python3.7 $HOME/venv/"$PYTHON_3_7_0_VERSION"
+virtualenv -p python3.7 $HOME/venv/"$PYTHON_VERSION"
 
 # Install pip packages
 . $HOME/venv/"$PYTHON_3_7_0_VERSION"/bin/activate
 pip install nose mock pytest coverage
 deactivate
-echo "================= Successfully Installed Python "$PYTHON_3_7_0_VERSION" ==================="
+echo "================= Successfully Installed Python "$PYTHON_VERSION" ==================="
